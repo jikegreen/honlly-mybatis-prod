ab5e314603adcd37de93f358e5a4e9df506af5a0
kwang89 <wangk@10.32.2.102>
2016-06-12 13:42:41 +0800

    File类修改

--- honlly-mybatis-parent/honlly-mybatis-generator/src/main/java/com/honlly/entity/utils/FileUtils.java
+++ honlly-mybatis-parent/honlly-mybatis-generator/src/main/java/com/honlly/entity/utils/FileUtils.java
@@ -2,21 +2,28 @@ package com.honlly.entity.utils;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileUtils {
 	public static void save(String path, String data) {
+		FileWriter out = null;
 		try {
 			File file = new File(path);
 			File dir = new File(path.substring(0, path.lastIndexOf("/")));
 			if(!dir.exists()) {
 				dir.mkdirs();
 			}
-			FileWriter out = new FileWriter(file);
+			out = new FileWriter(file);
 			out.write(data);
 			out.flush();
-			out.close();
 		} catch (Exception e) {
 			throw new RuntimeException(e);
+		} finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
